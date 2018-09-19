84ab066d8aecdfac55355739e375f4f06acf5947
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-19 18:31:33 +0000

    automatically backed up by learn

--- db/migrate/009_add_data_attributes_to_show.rb
+++ db/migrate/009_add_data_attributes_to_show.rb
@@ -1,4 +1,4 @@
-class AddDataAttributesToShows<ActiveRecord::Migration[4.2]
+class AddDataToShows<ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
