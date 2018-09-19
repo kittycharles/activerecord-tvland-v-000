eb366904808cc02f0f541b9e40d00034022f93dd
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-19 18:25:34 +0000

    automatically backed up by learn

--- db/migrate/007_add_catchphrase_to_characters.rb
+++ db/migrate/007_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
 class AddCatchphrasesToCharacters<ActiveRecord::Migration[4.2]
   def change
-    add_column :characters :catchphrase :string
+    add_column :characters, :catchphrase, :string
   end
 end
